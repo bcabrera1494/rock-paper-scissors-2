edb19531f2c3dd782b5f552ccc997073201c64fc
bcabrera1494 <b.cabrera1494@gmail.com>
2024-06-08 12:16:40 -0700

    Create computerChoice function
    Use math.Random*3 to create 3 numerical probability ranges to represent rock, paper, or scissors.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hello World!");
+console.log("Let's play rock, paper, scissors!");
+
+function getComputerChoice() {
+    let randomNumber = (Math.random()*3)
+    if (randomNumber <=1) {
+        return "Rock";
+    }
+    else if (randomNumber >=1 <=2 ) {
+        return "Paper";
+    }
+    else if (randomNumber >=2 <=3) {
+        return "Scissors";
+    }
+}
+
+let computerTurn = getComputerChoice();
+
+console.log(computerTurn);
+
+function getHumanChoice() {
+    let userInput = prompt("Choose rock, paper, or scissors" | "Type your choice here").toLowerCase();
+    switch (userInput) {
+        case "rock":
+            console.log("Rock");
+            break;
+        case "paper":
+            console.log("Paper")
+    }
+}
