fbef7165e9b6201c25cfb0c2662504a2749fd56e
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-21 18:26:34 -0700

    All unused code is commented out but kept for logic references later. Currently, the buttons for rock, paper, scissors are working as expected and the getComputerChoice function is working as expected. Need to find a way to contain the human choice based on button click in a variable for referenc e in the later alerts.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let computerScore = 0;
 //Append buttons to the body of the HTML
 const container = document.querySelector("body");
 //Create button for rock
-const rock = document.createElement('button')
+const rock = document.createElement('button');
 rock.textContent = 'Rock';
 //Create button for paper
 const paper = document.createElement('button');
@@ -23,30 +23,33 @@ container.appendChild(scissors);
 
 const buttons = document.querySelector('button');
 buttons.addEventListener('click', playRound());
- 
-function playRound() {
-        // let humanChoice = document.querySelectorAll('button');
-        // humanChoice.addEventListener('click', (event) => {
-        //     let target = event.target;
 
-        // switch(target.id) {
-        //     case 'rock':
-        //         console.log('Rock');
-        //         break;
-        //     case 'paper':
-        //         console.log('Paper');
-        //         break;
-        //     case 'scissors':
-        //         console.log('scissors');
-        //         break;
-        //     }
-        // });
-        paper.addEventListener('click', (e) => {
-            console.log('paper');
-        });
-        scissors.addEventListener('click', (e) => {
-            console.log('scissors');
-        })
+function playRound() {
+    // let humanChoice = document.querySelectorAll('button'); // This is the switch case logic that is supposed to delegate event. Doesn't work.
+    // humanChoice.addEventListener('click', (event) => {
+    //     let target = event.target;
+
+    // switch(target.id) {
+    //     case 'rock':
+    //         console.log('Rock');
+    //         break;
+    //     case 'paper':
+    //         console.log('Paper');
+    //         break;
+    //     case 'scissors':
+    //         console.log('scissors');
+    //         break;
+    //     }
+    // });
+    rock.addEventListener('click', (e) => {
+        console.log('rock');
+    })
+    paper.addEventListener('click', (e) => {
+        console.log('paper');
+    });
+    scissors.addEventListener('click', (e) => {
+        console.log('scissors');
+    });
 
     function getComputerChoice() {
         let randomNumber = (Math.random() * 3);
@@ -63,88 +66,66 @@ function playRound() {
 getComputerChoice();
     // alert(`You chose ${humanSelection}, Computer chose ${computerSelection}`);
     //     if (humanChoice === computerChoice) {
-        //         return console.log(`Tie!`); // alert
-        //     }
-        //     else if (humanChoice === "Rock" && computerChoice === "Paper") {
-        //         console.log(`You lose! Paper beats rock!`); // alert
-        //         getScore();
-        //         return console.log(`Human: ${humanScore}, Computer: ${computerScore}`); // alert
-        //     }
-        //     else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-        //         console.log(`You win! Rock beats scissors!`); // alert
-        //         getScore(); //alert
-        //         return console.log(`Human: ${humanScore}, Computer: ${computerScore}`);
-        //     }
-        //     else if (humanChoice === "Paper" && computerChoice === "Scissors") {
-        //         console.log(`You lose! Scissors beats paper!`); // alert
-        //         getScore();
-        //         return console.log(`Human: ${humanScore}, Computer: ${computerScore}`); // alert
-        //     }
-        //     else if (humanChoice === "Paper" && computerChoice === "Rock") {
-        //         console.log(`You win! Paper beats rock!`); // alert
-        //         getScore();
-        //         return console.log(`Human: ${humanScore}, Computer: ${computerScore}`); // alert
-        //     }
-        //     else if (humanChoice === "Scissors" && computerChoice === "Rock") {
-        //         console.log(`You lose! Rock beats scissors!`); // alert
-        //         getScore();
-        //         return console.log(`Human: ${humanScore}, Computer: ${computerScore}`); // alert
-        //     }
-        //     else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-        //         console.log(`You win! Scissors beats paper!`) // alert
-        //         getScore();
-        //         return console.log(`Human: ${humanScore}, Computer: ${computerScore}`); // alert
-        //     }
-        //     function getScore() {
-        //         if (humanChoice === "Rock" && computerChoice === "Paper"
-        //             || humanChoice === "Paper" && computerChoice === "Scissors"
-        //             || humanChoice === "Scissors" && computerChoice === "Rock") {
-        //             return ++computerScore
-        //         }
-        //         else if (humanChoice === "Rock" && computerChoice === "Scissors"
-        //             || humanChoice === "Paper" && computerChoice === "Rock"
-        //             || humanChoice === "Scissors" && computerChoice === "Paper") {
-        //             return ++humanScore
-        //         }
-        //     }
-        // }
-        //Remove logic that plays exactly 5 rounds per lesson directions        
-        // for (var round = 0; round < 5; ++round) {
-        //     playRound();
+    //         return console.log(`Tie!`); // alert
     //     }
-}
-//Append buttons to the body of the HTML
-const container = document.querySelector("body");
-//Create button for rock
-const rock = document.createElement('button')
-rock.textContent = 'Rock';
-//Create button for paper
-const paper = document.createElement('button');
-paper.textContent = 'Paper';
-//Create button for scissors
-const scissors = document.createElement('button');
-scissors.textContent = 'Scissors';
+    //     else if (humanChoice === "Rock" && computerChoice === "Paper") {
+    //         console.log(`You lose! Paper beats rock!`); // alert
+    //         getScore();
+    //         return console.log(`Human: ${humanScore}, Computer: ${computerScore}`); // alert
+    //     }
+    //     else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+    //         console.log(`You win! Rock beats scissors!`); // alert
+    //         getScore(); //alert
+    //         return console.log(`Human: ${humanScore}, Computer: ${computerScore}`);
+    //     }
+    //     else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+    //         console.log(`You lose! Scissors beats paper!`); // alert
+    //         getScore();
+    //         return console.log(`Human: ${humanScore}, Computer: ${computerScore}`); // alert
+    //     }
+    //     else if (humanChoice === "Paper" && computerChoice === "Rock") {
+    //         console.log(`You win! Paper beats rock!`); // alert
+    //         getScore();
+    //         return console.log(`Human: ${humanScore}, Computer: ${computerScore}`); // alert
+    //     }
+    //     else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+    //         console.log(`You lose! Rock beats scissors!`); // alert
+    //         getScore();
+    //         return console.log(`Human: ${humanScore}, Computer: ${computerScore}`); // alert
+    //     }
+    //     else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+    //         console.log(`You win! Scissors beats paper!`) // alert
+    //         getScore();
+    //         return console.log(`Human: ${humanScore}, Computer: ${computerScore}`); // alert
+    //     }
+    //     function getScore() {
+    //         if (humanChoice === "Rock" && computerChoice === "Paper"
+    //             || humanChoice === "Paper" && computerChoice === "Scissors"
+    //             || humanChoice === "Scissors" && computerChoice === "Rock") {
+    //             return ++computerScore
+    //         }
+    //         else if (humanChoice === "Rock" && computerChoice === "Scissors"
+    //             || humanChoice === "Paper" && computerChoice === "Rock"
+    //             || humanChoice === "Scissors" && computerChoice === "Paper") {
+    //             return ++humanScore
+    //         }
+    //     }
+    // }
+};
 
-container.appendChild(rock);
-container.appendChild(paper);
-container.appendChild(scissors);
 
-//Add event listener to buttons that call playRound function with the correct playerSelection 
-//every time the button is clicked (can keep console.log for this step)
 
-const buttons = document.querySelector('button');
-buttons.addEventListener('click', playRound());
 
 
 //Add a div for displaying results and change all console.log into DOM methods
 const results = document.createElement('div');
 container.appendChild(results)
-results.textContent = 'Score: ';
+results.textContent = 'Current Score:';
 //Display the running score, and announce a winner of the game (alert) once one player reaches
 //5 points
 //Will likely have to refactor original code to make it work
 
-playGame();
+// playGame();
 
 // function declareWinner(humanScore, computerScore) {
 //     if (humanScore === computerScore) {
